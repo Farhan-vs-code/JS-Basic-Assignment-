0b2d4feb348acb355ce74f88e77c9be50e62b386
Farhan-vs-code <farhintanvir8@gmail.com>
2024-05-31 21:58:17 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+// JavaScript Basic Problem Solving 1
+
+let carName = "Volvo";
+console.log(carName);
+
+//JavaScript Basic Problem Solving 2
+
+let  firstName = "John";
+let  lastName = "Doe";
+let   age = 35;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+//JavaScript Basic Problem Solving 3
+
+ x = 10;
+ y = 5;
+console.log(x *= y);// will be 50
+
+//JavaScript Basic Problem Solving 4
+
+let length = 16; // data type: number 
+let lastName2 = "Johnson"; // data type: string
+console.log(typeof length);
+console.log(typeof lastName2);
+
+const x2 = {
+    firstName: "John",  
+    lastName: "Doe"
+  };    // data type: object
+
+  console.log(typeof x2);
+
+//JavaScript Basic Problem Solving 5
+
+function myFunction() {
+    alert("Hello world!")
+};
+console.log(myFunction()); //output: Hello world!
+
+//JavaScript Basic Problem Solving 6
+
+const person = {
+    name: "John",
+    age: 50
+  };
+  
+  alert(`${person.name} is ${person.age}.`); 
+
+//JavaScript Basic Problem Solving 7
+
+function handleClick() {
+    alert("You clicked the button!");
+  }
+//JavaScript Basic Problem Solving 8 
+
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+cars[0] = "Ford";
+console.log(cars);
+
+
+//JavaScript Basic Problem Solving 9
+
+let randomNumber = Math.random();
+let maxNumber = Math.max(10,20);
+let squareNumber = Math.sqrt(9);
+
+console.log(randomNumber);
+console.log(maxNumber);
+console.log(randomNumber);
+
+//JavaScript Basic Problem Solving 10
+
+let x3 = 10
+let y2 = 5
+if (x > y);
+alert(x3 > y2)//output: true
+
+let age2 = 25;
+const message = age2 >= 18 ? "Old enough" : "Too young";
+alert(message);
+
